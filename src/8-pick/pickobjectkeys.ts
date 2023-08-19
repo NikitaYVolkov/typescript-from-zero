2ca165c017edfb51f8a520aa4163785b6d66a6e3
NikitaYVolkov <138996737+NikitaYVolkov@users.noreply.github.com>
2023-08-19 21:03:35 +0300

    Develop (#13)
    * Add readme
    
    * Add 17 fields with type
    
    * Add 3 fields with type
    
    * Add 3 fields with type
    
    * Add 1 field with type
    
    * Add 3 fields with type
    
    * Find and fix validation errors
    
    * Add tsconfig
    
    * First homework attempt
    
    * Update config
    
    * Update config
    
    * Add '5-map' homework, add gitignore
    
    * Add gitignore with generated js files
    
    * Add HashMap with 'set' method
    
    * Add HashMap implementation
    
    * Test HashMap in main.ts
    
    * Add build folder
    
    * Add build folder
    
    * Remove empty bucket when deleting
    
    * Add homework №7
    
    * Homework №8
    
    * Remove extra folders
    
    * main file
    
    * Homework №9
    
    ---------
    
    Co-authored-by: Nikita Volkov <gamment@gmail.com>
    Co-authored-by: Dmitriy Korbut <58600102+korbutds@users.noreply.github.com>

---
+++ src/8-pick/pickobjectkeys.ts
@@ -0,0 +1,26 @@
+function pickObjectKeys<T extends object, K extends keyof T>(obj: T, keys: K[]): { [property in K]: T[K] } {
+	type R = { [property in K]: T[K] };
+	return keys.reduce<R>((returnObj: R, key: K) => {
+		returnObj[key] = obj[key];
+		return returnObj;
+	}, {} as R);
+}
+
+export function pickObjectKeysTest() {
+	const user = {
+		name: "Vasiliy",
+		age: 8,
+		skills: ['typescript', 'javascript'],
+	};
+	console.log(user);
+
+	const userAgeAndSkillsValues = pickObjectKeys(user, ['age', 'skills']);
+	console.log(userAgeAndSkillsValues);
+
+	/*
+	res = {
+		age: 8,
+		skills: ['typescript', 'javascript']
+	}
+	*/
+}
