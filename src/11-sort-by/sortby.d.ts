e76f2d83f3f1e9e56d969a995f23bc4a459e543b
Nikita Volkov <gamment@gmail.com>
2023-08-20 21:13:25 +0300

    Add similar object, add func args

--- src/11-sort-by/sortby.d.ts
+++ src/11-sort-by/sortby.d.ts
@@ -1,5 +1,5 @@
 declare module 'sort-by' {
 	function type(type: string): (arg: any) => boolean;
-	function sort<V>(property: string, map: (property: string, value: V) => V): (a: object, b: object) => number;
-	export default function sortBy(...args: any[]): (obj1: object, obj2: object) => number;
+	function sort<V, T extends object>(property: string, map: (property: string, value: V) => V): (a: T, b: T) => number;
+	export default function sortBy<V, T extends object>(...args: (string | ((property: string, value: V) => V))[]): (obj1: T, obj2: T) => number;
 }
